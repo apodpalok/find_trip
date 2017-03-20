5d9751898bb49516ee3be21a1846d5e19d1f05fc
Anna <apodpalok@ukr.net>
2017-03-20 22:10:39 +0200

    add validates to trip model

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,2 +1,5 @@
 class Trip < ApplicationRecord
+  validates :start_location, presence: true 
+  validates :finish_location, presence: true
+  validates :price, presence: true, numericality: { only_integer: true }
 end
