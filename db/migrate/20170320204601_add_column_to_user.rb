c6a213d3dd7db846785eeed0cd96629a59fe12ee
Vitaliy Danilko <vitaliydanilko@gmail.com>
2017-03-20 22:49:21 +0200

    extend user add fields

---
+++ db/migrate/20170320204601_add_column_to_user.rb
@@ -0,0 +1,15 @@
+class AddColumnToUser < ActiveRecord::Migration[5.0]
+  def change
+      add_column :users, :first_name, :string
+      add_column :users, :last_name, :string
+      add_column :users, :birth_date, :date
+      add_column :users, :gender, :string
+      add_column :users, :phone_number, :integer
+      add_column :users, :mini_bio, :text
+      add_column :users, :address, :string
+      add_column :users, :address2, :string
+      add_column :users, :postcode, :integer
+      add_column :users, :city, :string
+      add_column :users, :country, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170320194911) do
+ActiveRecord::Schema.define(version: 20170320204601) do
 
   create_table "trips", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "start_location"
@@ -24,6 +24,34 @@ ActiveRecord::Schema.define(version: 20170320194911) do
     t.integer  "price"
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
+  end
+
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "email",                                default: "", null: false
+    t.string   "encrypted_password",                   default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",                        default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                                        null: false
+    t.datetime "updated_at",                                        null: false
+    t.string   "first_name"
+    t.string   "last_name"
+    t.date     "birth_date"
+    t.string   "gender"
+    t.integer  "phone_number"
+    t.text     "mini_bio",               limit: 65535
+    t.string   "address"
+    t.string   "address2"
+    t.integer  "postcode"
+    t.string   "city"
+    t.string   "country"
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
 end
