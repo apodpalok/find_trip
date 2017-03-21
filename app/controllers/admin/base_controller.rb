b60f672c1e2174db64d88091449912e1f203c598
Vitaliy Danilko <vitaliydanilko@gmail.com>
2017-03-21 13:54:00 +0200

    add admin side

---
+++ app/controllers/admin/base_controller.rb
@@ -0,0 +1,11 @@
+module Admin 
+  class BaseController < ApplicationController
+    before_action :authenticate_user!
+    before_action :only_admins
+
+    def only_admins
+      return if current_user.admin?
+      redirect_to root_path, alert: 'You have no permissions to access this page'
+    end
+  end
+end
