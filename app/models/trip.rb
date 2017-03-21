dd70743267a7fd1a42ad2f7a83a2e139b4ac7c3a
Anna <apodpalok@ukr.net>
2017-03-21 17:14:21 +0200

    fix bag in validates

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,5 +1,5 @@
 class Trip < ApplicationRecord
-  validates :start_location, presence: true 
+  validates :start_location, presence: true
   validates :finish_location, presence: true
   validates :price, presence: true, numericality: { only_integer: true }
 end
