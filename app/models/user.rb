2d7d34d2b41ac13f4e65c9e00c80661234514c24
Anna <apodpalok@ukr.net>
2017-04-01 21:30:04 +0300

    establish the relationship between user and trip

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,13 @@
 class User < ApplicationRecord
   has_many :cars
-  has_many :trips
-  mount_uploader :avatar, AvatarUploader
+  has_many :trip_memberships
+  has_many :trips, through: :trip_memberships
+  has_and_belongs_to_many :trips_as_driver, class_name: 'Trip', join_table: 'drivers_trips',
+                                            foreign_key: 'driver_id'
+  has_and_belongs_to_many :trips_as_passenger, join_table: 'passengers_trips',
+                                               foreign_key: 'passenger_id'
 
+  mount_uploader :avatar, AvatarUploader
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170326183329) do
+ActiveRecord::Schema.define(version: 20170401182203) do
 
   create_table "car_manufactories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "make"
@@ -34,11 +34,30 @@ ActiveRecord::Schema.define(version: 20170326183329) do
     t.datetime "updated_at",   null: false
   end
 
+  create_table "drivers_trips", id: false, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer "driver_id"
+    t.integer "trip_id"
+  end
+
   create_table "manufactory_car_models", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "car_manufactory_id"
     t.integer  "car_model_id"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
+  end
+
+  create_table "passengers_trips", id: false, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer "passenger_id"
+    t.integer "trip_id"
+  end
+
+  create_table "trip_memberships", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "trip_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["trip_id"], name: "index_trip_memberships_on_trip_id", using: :btree
+    t.index ["user_id"], name: "index_trip_memberships_on_user_id", using: :btree
   end
 
   create_table "trips", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -87,4 +106,6 @@ ActiveRecord::Schema.define(version: 20170326183329) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "trip_memberships", "trips"
+  add_foreign_key "trip_memberships", "users"
 end
