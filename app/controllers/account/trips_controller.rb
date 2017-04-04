410d4beb2e77b19a21cbd0176d481be2f54cf668
Anna <apodpalok@ukr.net>
2017-04-04 19:47:20 +0300

    add status to trips

--- app/controllers/account/trips_controller.rb
+++ app/controllers/account/trips_controller.rb
@@ -2,11 +2,21 @@ module Account
   class TripsController < BaseController
     before_action :find_trip, only: [:show]
 
-    def index
-      @trips = current_user.trips
-    end
+    def index; end
 
     def show; end
+
+    def active
+      @trips = current_user.trips.where(status: :active)
+      render :index
+    end
+
+    def archived
+      @trips = current_user.trips.where(status: :archived)
+      render :index
+    end
+
+    private
 
     def find_trip
       @trip = Trip.find(params[:id])
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170401182203) do
+ActiveRecord::Schema.define(version: 20170403140213) do
 
   create_table "car_manufactories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "make"
@@ -69,11 +69,12 @@ ActiveRecord::Schema.define(version: 20170401182203) do
     t.datetime "finish_time"
     t.float    "distance",         limit: 24
     t.integer  "price"
-    t.datetime "created_at",                  null: false
-    t.datetime "updated_at",                  null: false
+    t.datetime "created_at",                              null: false
+    t.datetime "updated_at",                              null: false
     t.float    "finish_latitude",  limit: 24
     t.float    "finish_longitude", limit: 24
     t.integer  "duration"
+    t.integer  "status",                      default: 0
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
