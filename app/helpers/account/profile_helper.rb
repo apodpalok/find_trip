c3c7b3754f03745f342be251eb887df9fcd17138
Vitaliy Danilko <vitaliydanilko@gmail.com>
2017-04-08 23:29:40 +0300

    add select active for account navigation bar

--- app/helpers/account/profile_helper.rb
+++ app/helpers/account/profile_helper.rb
@@ -1,12 +1,16 @@
 module Account
   module ProfileHelper
-    def avatar_for(user, options = { size: 80})
+    def avatar_for(user, options = { size: 80 }, name_class)
       size = options[:size]
       if user.avatar?
-        image_tag user.avatar.url(:thumb), width: size, class: "avatar-image" 
+        image_tag user.avatar.url(:thumb), width: size.to_i, class: name_class 
       else
-        image_tag "default-avatar.png", width: size, class: "avatar-image" 
+        image_tag "default-avatar.png", width: size.to_i, class: name_class 
       end
+    end
+
+    def navigation_params
+      { profile: false, trip: false, car: false, photo: false, messages: false }
     end
   end
 end
