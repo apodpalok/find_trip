8cc3625a826aab4ef8d3aec3c67496d1cb96b024
Vitaliy Danilko <vitaliydanilko@gmail.com>
2017-04-10 13:23:42 +0300

    update views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def avatar_for(user, options = { size: 250 })
+    size = options[:size]
+    if user.avatar?
+      image_tag user.avatar.url(:thumb), width: size.to_i 
+    else
+      image_tag "default-avatar.png", width: size.to_i
+    end
+  end
 end
