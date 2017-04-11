db7d2d27ececed8eedef2fee740a61ac61de2769
Vitaliy Danilko <vitaliydanilko@gmail.com>
2017-04-11 23:23:51 +0300

    add converstations

--- app/controllers/account/base_controller.rb
+++ app/controllers/account/base_controller.rb
@@ -1,11 +1,5 @@
 module Account
   class BaseController < ApplicationController
     before_action :authenticate_user!
-
-    private
-
-    def not_found
-      raise ActionController::RoutingError, 'Page Not Found'
-    end
   end
 end
