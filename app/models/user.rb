dd3edfeb4457499516e62761eb61ebbadc777f49
Vitaliy Danilko <vitaliydanilko@gmail.com>
2017-04-14 13:15:00 +0300

    add sign in with facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,11 @@ class User < ApplicationRecord
                                             foreign_key: 'driver_id'
   has_and_belongs_to_many :trips_as_passenger, join_table: 'passengers_trips',
                                                foreign_key: 'passenger_id'
+  has_many :identities
 
   mount_uploader :avatar, AvatarUploader
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :omniauthable, :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable
 
   acts_as_messageable
 
@@ -25,5 +26,13 @@ class User < ApplicationRecord
 
   def full_name
     [first_name, last_name].join(' ')
+  end
+
+  def facebook
+    identities.where( :provider => "facebook" ).first
+  end
+
+  def facebook_client
+    @facebook_client ||= Facebook.client( access_token: facebook.accesstoken )
   end
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -276,4 +276,5 @@ Devise.setup do |config|
   # When using OmniAuth, Devise cannot automatically set OmniAuth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
+  config.omniauth :facebook, "406046646437036", "fc6127534b5b169f03d35135ff41637c", scope: "email", setup: true
 end
