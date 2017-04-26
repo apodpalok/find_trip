33dfcf5c4f0984521c7e16ae99ad20920988925f
vitaliydanilko <vitaliydanilko@gmail.com>
2017-04-26 11:34:40 +0300

    [+] include all /supports files to rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,14 +8,11 @@ require 'spec_helper'
 require 'rspec/rails'
 require 'devise'
 require 'factory_girl'
-require 'support/factory_girl'
-require 'support/controller_macros'
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
-  config.include Devise::Test::ControllerHelpers, type: :controller
-  config.include ControllerMacros, type: :controller
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
